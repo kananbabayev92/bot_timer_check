30b805853fbaf27351963612a5309c8f9e7c0238
Kanan Babayev <39367332+kananbabayev92@users.noreply.github.com>
2025-03-08 23:11:53 +0400

    Update telegram_token.py

--- telegram_token.py
+++ telegram_token.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> 706a9f5b157228af85078250545ab3ed3d840e1a
 FIRST_MESSAGE = """🙌Salam! Mənimlə işləmək üçün /start_work komandasını istifadə edə bilərsən.
 ⏲️Saatı görmək üçün /show_time komandasını istifadə ede bilərsən"""
